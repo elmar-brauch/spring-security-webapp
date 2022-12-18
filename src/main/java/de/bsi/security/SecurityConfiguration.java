e0a71310bc36f60251c57f1ab360a6e8ef40db9d
Elmar Brauch <elmar.brauch@gmail.com>
2022-12-18 21:08:41 +0100

    Update to Spring Security 6 with Spring Boot 3

--- src/main/java/de/bsi/security/SecurityConfiguration.java
+++ src/main/java/de/bsi/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package de.bsi.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.*;
@@ -10,15 +11,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN")
+		http.authorizeHttpRequests()
+			.requestMatchers("/admin").hasRole("ADMIN")
 			.anyRequest().permitAll()
-			.and().formLogin()
-			.and().logout().logoutSuccessUrl("/index");
+		.and().formLogin()
+		.and().logout().logoutSuccessUrl("/index");
 		return http.build();
 	}
 	
